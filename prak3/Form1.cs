7e2b7bfd29ab8ed6c3c5c8262e97c9e33649461a
taznimt <taznimanwar06@gmail.com>
2025-03-21 01:10:38 +0700

    Memastikan bahwa baris yang diklik adalah baris yang valid (bukan header atau area kosong)

--- prak3/Form1.cs
+++ prak3/Form1.cs
@@ -185,7 +185,10 @@ namespace prak3
 
         private void dgvMahasiswa_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+            if (e.RowIndex >= 0)
+            {
 
+            }
         }
     }
 
