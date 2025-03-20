1247c087c5c46cd11da91e399050a2b989ce1a0d
taznimt <taznimanwar06@gmail.com>
2025-03-21 00:37:46 +0700

    Memeriksa apakah ada field yang kosong sebelum menambahkan data

--- prak3/Form1.cs
+++ prak3/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -67,7 +68,13 @@ namespace prak3
         {
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
+                try
+                {
+                    if (labelNIM.Text == "" || labelNama.Text == "" || labelEmail.Text == "" || labelTelepon.Text == "")
+                    {
 
+                    }
+                }
             }
         }
     }
