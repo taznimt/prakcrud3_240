492a91dd9a06a1612bad974aecf26e47acce8b79
taznimt <taznimanwar06@gmail.com>
2025-03-21 00:49:17 +0700

    Menampilkan pesan error dalam bentuk MessageBox

--- prak3/Form1.cs
+++ prak3/Form1.cs
@@ -111,7 +111,10 @@ namespace prak3
                         }
                     }
                 }
-                catch (Exception ex) 
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
         }
     }
